f3ffd61877e77373d8214a6253e3e2a8c35c613e
DanielW114 <158521428+DanielW114@users.noreply.github.com>
2024-04-18 20:52:16 +0000

    Completed Work

--- domManipulation.js
+++ domManipulation.js
@@ -22,24 +22,24 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+  myName.innerHTML = "My name is Daniel";
+  myHobby.innerHTML = "I like to sleep";
+  myLocation.innerHTML = "I live in Queens";
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
 
-
-
-
+image1Button.addEventListener('mouseover', function() { 
+  image.src = "https://static.vecteezy.com/system/resources/thumbnails/025/220/125/small_2x/picture-a-captivating-scene-of-a-tranquil-lake-at-sunset-ai-generative-photo.jpg"
+});
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
 
+image2Button.addEventListener('dblclick', function (){
+  image.src = "https://www.davelabowitz.com/wp-content/uploads/Sisyphus-e1557869810488.jpg"
+});
 
 // BONUS
 // 7. Try using different event types for your buttons.
